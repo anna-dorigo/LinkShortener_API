fa4019813c43dd69efa7b118a118e87b28b160fd
anna-dorigo <alejandr@gmail.com>
2020-06-10 00:03:34 -0400

    enviromental variable set up and create, delete and show

--- app/controllers/v1/links_controller.rb
+++ app/controllers/v1/links_controller.rb
@@ -6,4 +6,59 @@ class V1::LinksController < ApplicationController
 		render json:@links, status: :ok
 	end
 
+	def show
+		@link = Link.find(params[:id])
+		puts "-------------------------"
+		puts ENV["DOMAIN_URL"]
+    	render json: @link, :methods => :shortened_url
+  	end
+
+	def create
+
+		original_url = params[:original_url]
+	    slug = params[:slug]
+	    lookup_code_string = lookup_code()
+
+	    if slug == ""
+	    	@link = Link.create(original_url: original_url, lookup_code: lookup_code_string)
+	    else
+	    	@link = Link.create(original_url: original_url, lookup_code: lookup_code_string, slug: slug)
+	    end 
+
+	    if @link.save
+	    	render json: @link, status: :created
+	    else
+	    	render json: @link.errors, status: :unprocessable_entity
+	    end
+	end
+
+	def destroy
+		@link = Link.where(id: params[:id]).first
+
+		if @link.destroy
+	    	head(:ok)
+	    else
+	    	render json: @link.errors, status: :unprocessable_entity
+	    end
+	end
+
+
+
+	private
+
+	def lookup_code
+    	loop do
+    		code = get_fresh_code()
+     		break code unless Link.exists?(lookup_code: code)
+    	end 
+  	end
+
+	def check_params
+		params.require(:original_url).permit(:slug)
+	end
+
+	def get_fresh_code()
+    	"_"+SecureRandom.uuid[0..5]
+  	end
+
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,22 @@
 class Link < ApplicationRecord
+	validates_presence_of :lookup_code, :original_url
+	validates_uniqueness_of :lookup_code
+	validate :original_url_format
+	validates :slug, uniqueness: true, allow_nil: true
+
+	def original_url_format
+		uri = URI.parse(original_url || "")
+		if uri.host.nil?
+			errors.add(:original_url, "Invalid URL format")
+		end
+
+	end
+
+	def shortened_url
+		if slug.nil?
+			ENV["DOMAIN_URL"]+lookup_code
+		else
+			ENV["DOMAIN_URL"]+slug
+		end
+	end
 end
