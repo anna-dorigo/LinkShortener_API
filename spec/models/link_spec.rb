6f263f05dc1aafbd03977525e4e4912d2bae392a
anna-dorigo <alejandr@gmail.com>
2020-06-11 00:08:41 -0400

    Add tests

---
+++ spec/models/link_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe Link, type: :model do
+ 	it "is valid if it has an orginal URL and a short code" do
+		link = Link.new(
+				original_url: "https://www.favoritewebsite.com/articles/how-to-cook",
+				lookup_code: "1234567"
+			)
+		expect(link.valid?).to be(true)
+	end
+
+	it "is invalid if the URL is not formatted properly" do
+		link = Link.new(
+				original_url: "iueiueiqueo",
+				lookup_code: "1234567"
+			)
+		expect(link.valid?).to be(false)
+	end
+
+	it "is invalid if does not have a lookup code" do
+		link = Link.new(
+				original_url: "https://www.favoritewebsite.com/articles/how-to-cook"
+			)
+		expect(link.valid?).to be(false)
+	end
+
+	it "is invalid if does not have an original url" do
+		link = Link.new(
+				lookup_code: "1234567"
+			)
+		expect(link.valid?).to be(false)
+	end
+
+	it "is invalid if the lookup code already exists" do
+		link = Link.new(
+				original_url: "https://www.favoritewebsite.com/articles/how-to-cook",
+				lookup_code: "1234567"
+			)
+		link.save
+
+		link2 = Link.new(
+				original_url: "https://www.favoritewebsite.com/articles/how-to-bake",
+				lookup_code: "1234567"
+			)
+		expect(link2.valid?).to be(false) 
+	end
+
+end
